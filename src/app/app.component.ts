a09221fca3aebff77b22aa3f656a28bc2af1fbfc
Gianella Guarda <guarda.fernandez@hotmail.com>
2021-01-13 10:23:31 -0500

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular';
+
+  curso: String = "Curso Spring con Angular";
+  profesor: String = "Gianella Guarda";
+}
